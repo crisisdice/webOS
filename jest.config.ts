754c8ed30cc5992a7d2360113f4a18e564912a87
Alexander Daily <alexanderdaily001@gmail.com>
2022-10-31 23:40:49 +0100

    ui tests

--- jest.config.ts
+++ jest.config.ts
@@ -154,8 +154,7 @@ export default {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    // "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
+    "**/__tests__/state/**/*.[jt]s?(x)",
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
